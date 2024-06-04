7377c1a0a469105f3a513591748bed66b8f2c7db
Vicous <vicous@free.fr>
2024-06-04 19:53:07 +0200

    add launch script

--- launch.sh
+++ launch.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
 echo "boom ca lance tout"
+#!/bin/bash
+
+# List of directories
+directories=("jenkins" "portainer" "nginx-proxy-manager")
+
+# Loop through each directory and run docker-compose up --build
+for dir in "${directories[@]}"; do
+  if [ -d "$dir" ]; then
+    echo "Changing to directory: $dir"
+    cd "$dir" || exit
+    echo "Running docker-compose up --build in $dir"
+    docker compose up --build -d
+    echo "Completed docker-compose up --build in $dir"
+    cd "../"
+  else
+    echo "Directory $dir does not exist"
+  fi
+done
+
+echo "Script completed."
